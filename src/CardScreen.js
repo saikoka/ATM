e76294b8acbffaa2ff3364cd0594c08fc78f7c00
Sai Koka <saikoka@proton.me>
2024-06-08 16:08:48 +0000

    Started form input for Card Screen

--- src/CardScreen.js
+++ src/CardScreen.js
@@ -1,9 +1,31 @@
 export default function CardScreen(props) {
+  const checkCard = () => {};
   return (
     <>
-      <h1>Welcome!</h1>
-      <h2>Please enter card number to begin</h2>
-      <input type="number" minLength={16} maxLength={16}></input>
+      <div id="card-screen">
+        <h1 className="bottom-spacing">Welcome</h1>
+        <h3>Please enter card number and pin to begin</h3>
+        <form onSubmit={checkCard}>
+          <input
+            type="number"
+            minlength={16}
+            maxlength={16}
+            placeholder="Card Number"
+            className="form-sizing"
+          ></input>
+
+          <input
+            type="number"
+            minLength={4}
+            maxlength={4}
+            placeholder="PIN"
+            className="form-sizing"
+          ></input>
+          <button type="submit" id="" className="form-sizing">
+            Next
+          </button>
+        </form>
+      </div>
     </>
   );
 }
